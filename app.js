ba9f20c0bf880e307e6e23f0536137b49d03e033
ofranco1205-7409 <60754342+ofranco1205-7409@users.noreply.github.com>
2022-08-17 18:18:52 -0500

    seccion-7

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
-const { API_VERSION } = require("./config");
+const app = express()
+const { API_VERSION } = require('./config')
 
 // Load routings
+const userRoutes = require('./routers/user')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Configure Header HTTP
 
 // Router Basic
+app.use(`/api/${API_VERSION}`, userRoutes)
 
-module.exports = app;
+module.exports = app
---
+++ routers/user.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const UserController = require('../controllers/user')
+
+const api = express.Router()
+
+api.post('/signUp', UserController.signUp)
+
+module.exports = api
