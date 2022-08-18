e456c812cab3533ac9b08245d335fa99a28d9a96
ofranco1205-7409 <60754342+ofranco1205-7409@users.noreply.github.com>
2022-08-18 14:02:19 -0500

    seccion-7-cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+var cors = require('cors')
 
 const app = express()
 const { API_VERSION } = require('./config')
@@ -7,10 +8,24 @@ const { API_VERSION } = require('./config')
 // Load routings
 const userRoutes = require('./routers/user')
 
+app.use(cors())
+//app.use(function (req, res, next) {
+//  res.header('Access-Control-Allow-Origin', 'localhost') // update to match the domain you will make the request from
+//  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//  next()
+//})
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Configure Header HTTP
+/*
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})*/
 
 // Router Basic
 app.use(`/api/${API_VERSION}`, userRoutes)
